68114f2396a304d9d3008d7a8d82a4e6d8f2d321
kaypappi <wesleyukadike@yahoo.com>
2019-08-12 13:50:16 +0100

    Add files via upload

---
+++ scripts/app.js
@@ -0,0 +1,37 @@
+const form = document.querySelector('form')
+const card = document.querySelector('.holder')
+console.log()
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    updatedetails(form.city.value)
+    .then(()=> form.city.value='').catch(err=> console.log(err))
+
+})
+
+const updatedetails = async (search) => {
+    getWeather(search)
+        .then(response => {
+            let { Details, weatherDet } = response
+            console.log(Details,weatherDet)
+            let sky=`${weatherDet.IsDayTime? 'day.svg': 'night.svg'}`
+            let html = `<img name='card'src="../img/${sky}" alt="" class="time card-img-top">
+        <div class="icon bg-light mx-auto text-center">
+            <img src='../img/icons/${weatherDet.WeatherIcon}.svg'>
+        </div>
+        <div class="text-muted text-uppercase text-center details">
+            <h5 class="my-3">${Details.EnglishName}</h5>
+            <div class="my-3">${weatherDet.WeatherText}</div>
+            <div class="display-4 my-4">
+                <span>${weatherDet.Temperature.Metric.Value}</span>
+                <span>&deg;C</span>
+            </div>
+        </div>`
+
+        if(card.classList.contains('d-none')){
+            card.classList.remove('d-none')
+        }
+
+        return card.innerHTML= html
+        }).catch(err=>console.log(err))
+}
