1b1e4c40fca7be95b00874eff1b8a4e615fd1361
kaypappi <wesleyukadike@yahoo.com>
2019-08-12 15:57:25 +0100

    Update weather.js

--- scripts/weather.js
+++ scripts/weather.js
@@ -1,7 +1,7 @@
 const key='d9G9Ow6izgYySrxflVKSw8AnIJgCMP8o'
 
 const getLocation=async(search)=>{
-    let query=`http://dataservice.accuweather.com/locations/v1/cities/search?apikey=${key}&q=${search}`
+    let query=`https://dataservice.accuweather.com/locations/v1/cities/search?apikey=${key}&q=${search}`
     let cityDetails= await fetch(query)
     let newDetails= await cityDetails.json()
     return newDetails[0]
@@ -11,7 +11,7 @@ const getLocation=async(search)=>{
 const getWeather=async(search)=>{
    let Details=await getLocation(search)
    let cityKey=Details.Key
-   let query2=`http://dataservice.accuweather.com/currentconditions/v1/${cityKey}?apikey=${key}`
+   let query2=`https://dataservice.accuweather.com/currentconditions/v1/${cityKey}?apikey=${key}`
    let request= await fetch(query2)
    let weatherDet=await request.json()
    weatherDet=weatherDet[0]
